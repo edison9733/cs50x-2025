85053eaeffde190e2041e2c8d4b0bd9fb8f300c2
edison9733 <eliu4864@gmail.com>
2025-06-30 07:02:49 +0000

    Mon, Jun 30, 2025, 3:02 PM +08:00

---
+++ Practice_make_Permanent/readability1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+
+int main(void)
+{
+    // Prompt user for text input
+    string text = get_string("Text: ");
+
+    // Count letters, words, and sentences in the text
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Calculate the Coleman-Liau index
+    
+}
+
+int count_letters(string text)
+{
+
+}
+
+int count_words(string text)
+{
+
+}
+
+int count_sentences(string text)
+{
+
+}
