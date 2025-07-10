d0e02457c0663e8148031963b38197192301852b
edison9733 <eliu4864@gmail.com>
2025-07-10 10:05:27 +0000

    Thu, Jul 10, 2025, 6:05 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -10,11 +10,35 @@ def count_letters(text):
             count += 1
     return count
 
+def count_words(text):
+    count = 0
+    for word in text:
+        if word.isspace() or word == ' ':
+            count += 1
+    return count + 1  # Add one for the last word
+
+def count_sentences(text):
+    count = 0
+    for char in text:
+        if char == '.' or char == '!' or char == '?':
+            count += 1
+    return count
+
 sum = 0
 
 letters = count_letters(text)
+words = count_words(text)
+sentences = count_sentences(text)
 
+L = letters / words * 100
+S = sentences / words * 100
+index = 0.0588 * L - 0.296 * S - 15.8
 
+if index < 1:
+    print("Before Grade 1")
+elif index >= 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {round(index)}")
 
-print({count_letters(text)}, letters)
 
