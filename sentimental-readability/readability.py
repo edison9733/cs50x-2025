b151ce9f6bbdba09a2bd3b36b7de936666d66ac5
edison9733 <eliu4864@gmail.com>
2025-07-10 09:59:44 +0000

    Thu, Jul 10, 2025, 5:59 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,19 +2,19 @@
 
 text = input("Text: ")
 
-sum = 0
-
-letters = count_letters(text)
-words = count_words(text)
-sentences = count_sentences(text)
-
 
 def count_letters(text):
     count = 0
-    for char in range(text):
+    for char in text:
         if char.isalpha():
             count += 1
     return count
+
+sum = 0
+
+letters = count_letters(text)
+
+
 
 print({count_letters(text)}, letters)
 
