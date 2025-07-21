f6bb9a989a752a732d694d42434f1fbf812c2be3
edison9733 <eliu4864@gmail.com>
2025-07-21 10:08:02 +0000

    Mon, Jul 21, 2025, 6:08 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -25,6 +25,15 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Inssert info into db table
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (:name, :month, :day),
+            name = name,
+            month = month,
+            day = day)
 
         # TODO: Add the user's entry into the database
 
