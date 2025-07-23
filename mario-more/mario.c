2b54365cc243fffac712d4cf9ae6e0e27a1bfca9
edison9733 <eliu4864@gmail.com>
2025-07-23 07:31:03 +0000

    Wed, Jul 23, 2025, 3:31 PM +08:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,34 +3,25 @@
 
 int main(void)
  {
-    int number = get_int("Height: ");
-    if (number <= 0)
+   // Get input of the Height
+   int n;
+   do
+   {
+    n = get_int("Height: ");
+   }
+   while ( n < 1 || n > 8);
+
+   // Print desired pyramid height
+   for (int i = 0; i < n; i++)
+   {
+    // Set perimeters for thee columns to print
+    for (int j = 0; j < n+i+3; j++)
     {
-        printf("Please enter a positive height.\n");
-        return 1;
+        if ( j ==n || j == n+1 || j+i < n-1)
+            printf(" ");
+        else
+            printf("#");
     }
-    else
-    {
-        for (int i = 0; i < number; i++)
-        {
-            // Print spaces
-            for (int a = 0; a < number - i; a++)
-            {
-                printf(" ");
-            }
-            // Print left side of the pyramid
-            for (int j = 0; j <= i ; j++)
-            {
-                printf("#");
-            }
-            // Print gap between the two sides
-            printf("  ");
-            // Print right side of the pyramid
-            for (int o = 0 ; o < i + 1; o++)
-            {
-                printf("#");
-            }
-            printf("\n");
-        }
-    }
+    printf("\n");
+   }
 }
--- me/hello.c
+++ me/hello.c
@@ -4,6 +4,6 @@
 int main(void)
 {
     printf("Hello, World!\n");
-    string name = get_string("What is your name?\n");
-    printf("Hello, %s!\n", name);
+    string name = get_string("What is your name? ");
+    printf("Hello, %s\n", name);
 }
